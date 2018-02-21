a9b36220882fa4565d803be58fb76d8a941ed967
icecandy8 <icecandy8@users.noreply.github.com>
2018-02-21 22:31:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,33 @@
-console.log('Loaded!');
+// console.log('Loaded!');
 
-//Change the text of the main-text div
-var element = document.getElementById("main-text");
-element.innerHTML = "New Value";
+// //Change the text of the main-text div
+// var element = document.getElementById("main-text");
+// element.innerHTML = "New Value";
 
-//Move the image
-var img = document.getElementById('madi');
-// var marginLeft = 0;
-// function moveRight(){
-//     marginLeft = marginLeft+1;
-//     img.style.marginLeft = marginLeft + "px";
-// }
-img.onClick = function () {
-    //Every 100ms apply the move right function
-    // var interval = setInterval(moveRight, 50);
-    img.style.marginRight = '100px';  
+// //Move the image
+// var img = document.getElementById('madi');
+// // var marginLeft = 0;
+// // function moveRight(){
+// //     marginLeft = marginLeft+1;
+// //     img.style.marginLeft = marginLeft + "px";
+// // }
+// img.onClick = function () {
+//     //Every 100ms apply the move right function
+//     // var interval = setInterval(moveRight, 50);
+//     img.style.marginRight = '100px';  
+// };
+
+//Counter Code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onClick = function(){
+  //Make a request to the counter endpoint
+  
+  //Capture the response and store it in a variable
+  
+  //Render the variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('counter');
+  span.innerHTML = counter.toString();
 };
