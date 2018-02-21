c579c1df585c624aa12bae9e43182493765300cc
icecandy8 <icecandy8@users.noreply.github.com>
2018-02-21 21:20:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,8 @@ var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 
 //Move the image
-var img = document.getElementById("madi")
+var img = document.getElementById("madi");
 img.onClick = function () {
-    img.style.marginLeft = '300px';  
+    var interval = setInterval(moveLeft, 100);
+    img.style.marginLeft = '100px';  
 };
