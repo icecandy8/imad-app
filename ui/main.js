7f742a5453b18c8f43cae2b7a7eb7f1aa0c97628
icecandy8 <icecandy8@users.noreply.github.com>
2018-02-21 21:26:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,11 @@ element.innerHTML = "New Value";
 var img = document.getElementById("madi");
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft+10;
+    marginLeft = marginLeft+1;
     img.style.marginLeft = marginLeft + "px";
 }
 img.onClick = function () {
     //Every 100ms apply the move right function
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
     // img.style.marginLeft = '100px';  
 };
